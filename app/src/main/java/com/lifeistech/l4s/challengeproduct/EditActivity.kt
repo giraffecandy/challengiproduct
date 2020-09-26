e3ace5c86d545479e07122f3697e9c3f9f126653
lifeistech <[64596612+giraffecandy@users.noreply.github.com]>
2020-09-26 19:43:54 +0900

    Revert "詳細画面から編集画面で編集画面に値が入った"
    This reverts commit 0dd68c0b

--- app/src/main/java/com/lifeistech/l4s/challengeproduct/EditActivity.kt
+++ app/src/main/java/com/lifeistech/l4s/challengeproduct/EditActivity.kt
@@ -2,11 +2,15 @@ package com.lifeistech.l4s.challengeproduct
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.format.DateUtils
 import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import io.realm.Realm
+import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.activity_edit.*
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -14,10 +18,6 @@ import java.util.*
 class EditActivity : AppCompatActivity() {
 
     private val realm: Realm = Realm.getDefaultInstance()
-
-    val acceptData: String? = intent.getStringExtra("GO_EDIT")
-    var getId: Book? = realm.where(Book::class.java).equalTo("id", acceptData).findFirst()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
@@ -26,35 +26,29 @@ class EditActivity : AppCompatActivity() {
         val book: Book? = read()
 
 
+        val acceptData:String? = intent.getStringExtra("GO_EDIT")
 
         Log.d("accept", acceptData.toString())
 //    var selectedData = realm.where(Book::class.java).equalTo("id", acceptData).findFirst()
 
 
 //        Log.d("selecte", selectedData.toString())
-//        if (acceptData != null) {
+        if (acceptData != null) {
+            var getId: Book? =
+                realm.where(Book::class.java).equalTo("id", acceptData).findFirst()
 //            Log.d("data", getId.toString())
-        if (getId != null) {
-            titleEditText.setText(getId!!.title)
-            autherEditText.setText(getId!!.auther)
-            priceEditText.setText(getId!!.price.toString())
-            descriptionEditText.setText(getId!!.description)
+            val titleEdit= getId?.title
+            if (getId != null) {
+                titleEditText.setText(getId.title)
+                    autherEditText.setText(getId.auther)
+                    priceEditText.setText(getId.price.toString())
+                descriptionEditText.setText(getId.description)
 
-//                addButton.setOnClickListener {
-//
-//                    val title: String = titleEditText.text.toString()
-//                    val auther: String = autherEditText.text.toString()
-//                    val description: String = descriptionEditText.text.toString()
-//                    val price: String = priceEditText.text.toString()
-//                    val getTime = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
-//
-//                    realm.commitTransaction()
-//                    getId!!.title = title
-//                    getId!!.auther = auther
-//                    getId.description = description
-//                    getId.price = price.toInt()
-//                }
-        }
+                addButton.setOnClickListener {
+
+                    realm.commitTransaction()
+                }
+                }
 //                else {
 
 //                titleEditText.text = null
@@ -63,7 +57,7 @@ class EditActivity : AppCompatActivity() {
 //                descriptionEditText.text = null
 //
 //        }
-//    }
+    }
 
 //            titleEditText.setText(book?.title)
 //            autherEditText.setText(book?.auther)
@@ -74,7 +68,7 @@ class EditActivity : AppCompatActivity() {
 //        val imageButton = backButton(this)
 //        imageButton.setBackgroundDrawable(null)
 
-        backButton.setOnClickListener {
+                backButton.setOnClickListener {
 //            val title: String = titleEditText.text.toString()
 //            val auther: String = autherEditText.text.toString()
 //            val description: String = descriptionEditText.text.toString()
@@ -83,17 +77,17 @@ class EditActivity : AppCompatActivity() {
 //
 //            save(title, auther, description, price, getTime)
 
-            val intent = Intent(applicationContext, MainActivity::class.java)
-            startActivity(intent)
-        }
+                    val intent = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(intent)
+                }
 
-        addButton.setOnClickListener {
+                addButton.setOnClickListener {
 //            if(realm.where(Book::class.java).equalTo("id", id) == null)
-            val title: String = titleEditText.text.toString()
-            val auther: String = autherEditText.text.toString()
-            val description: String = descriptionEditText.text.toString()
-            val price: String = priceEditText.text.toString()
-            val getTime = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+                    val title: String = titleEditText.text.toString()
+                    val auther: String = autherEditText.text.toString()
+                    val description: String = descriptionEditText.text.toString()
+                    val price: String = priceEditText.text.toString()
+                    val getTime = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
 //                    val sdf =
 //                        SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'")
 //                    sdf.timeZone = TimeZone.getTimeZone("Asia/Tokyo")
@@ -117,60 +111,52 @@ class EditActivity : AppCompatActivity() {
 //                        DateUtils.MINUTE_IN_MILLIS
 //                    ) as String
 
-            save(title, auther, description, price, getTime)
+                    save(title, auther, description, price, getTime)
 
-            val intent = Intent(applicationContext, MainActivity::class.java)
-            startActivity(intent)
-        }
+                    val intent = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(intent)
+                }
 
-
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        realm.close()
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id: Int = item.getItemId()
-        when (id) {
-            android.R.id.home -> finish()
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun read(): Book? {
-        return realm.where(Book::class.java).findFirst()
-    }
-
-    fun save(
-        title: String,
-        auther: String,
-        description: String,
-        price: String,
-        getTime: SimpleDateFormat
-//                sdf: SimpleDateFormat
-    ) {
-        realm.executeTransaction {
-            if (getId != null) {
-
-                getId!!.title = title
-                getId!!.auther = auther
-                getId!!.description = description
-                getId!!.price = price.toInt()
-
-            } else {
-                val book = it.createObject(Book::class.java, UUID.randomUUID().toString())
-                book.title = title
-                book.auther = auther
-                book.description = description
-                book.price = price.toInt()
-//                    book.time = sdf.toInt()
-//                    book.time = getTime.toInt()
 
             }
-        }
-    }
 
-}
+            override fun onDestroy() {
+                super.onDestroy()
+                realm.close()
+            }
+
+            override fun onOptionsItemSelected(item: MenuItem): Boolean {
+                val id: Int = item.getItemId()
+                when (id) {
+                    android.R.id.home -> finish()
+                }
+                return super.onOptionsItemSelected(item)
+            }
+
+            private fun read(): Book? {
+                return realm.where(Book::class.java).findFirst()
+            }
+
+            fun save(
+                title: String,
+                auther: String,
+                description: String,
+                price: String,
+                getTime: SimpleDateFormat
+//                sdf: SimpleDateFormat
+            ) {
+                realm.executeTransaction {
+
+
+                    val book = it.createObject(Book::class.java, UUID.randomUUID().toString())
+                    book.title = title
+                    book.auther = auther
+                    book.description = description
+                    book.price = price.toInt()
+//                    book.time = sdf.toInt()
+//                    book.time = getTime.toInt()
+                }
+            }
+
+        }
 
